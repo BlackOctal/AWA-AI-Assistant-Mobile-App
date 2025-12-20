535402c51b061bcccfde1c2fe149e897f16eaca2
BlackOctal <janiduhwelarathna@gmail.com>
2025-12-21 00:45:48 +0530

    Initial Commit

---
+++ AWA/AWAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWAApp.swift
+//  AWA
+//
+//  Created by Janidu Himansa on 2025-12-21.
+//
+
+import SwiftUI
+
+@main
+struct AWAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
